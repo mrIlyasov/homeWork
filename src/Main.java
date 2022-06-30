bd1afebd1e0d69c80fc80f88a501673acecc4247
Артур Ильясов <mr.ilyasov90@yandex.ru>
2022-06-30 22:01:40 +0500

    cicle added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int[] ints = new int[10];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = i + 1;
+            System.out.print(ints[i]+" ");
+        }
     }
 }
